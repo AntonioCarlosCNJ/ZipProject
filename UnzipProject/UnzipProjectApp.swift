562935375ea492fcd96af88c1395068b5c3846bf
Antonio Carlos <antonio.correa0407@gmail.com>
2022-01-02 22:36:26 -0300

    Initial Commit

---
+++ UnzipProject/UnzipProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnzipProjectApp.swift
+//  UnzipProject
+//
+//  Created by Antonio Carlos on 02/01/22.
+//
+
+import SwiftUI
+
+@main
+struct UnzipProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
